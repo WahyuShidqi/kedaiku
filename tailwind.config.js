3a8d6e2a4e16f2920f3feae02da4752335082ecd
Wahyu Shidqi <wahyushidqi550@gmail.com>
2023-03-30 16:43:39 +0700

    membuat tampilan navbar dan homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        dada: {
+          "0%": { opacity: "0", },
+          "2%": { opacity: "0", transform: "translateY(-30px)"},
+          "5%": { opacity: "1", transform: "translateY(0px)" },
+          "17%": { opacity: "1", transform: "translateY(0px)" },
+          "20%": { opacity: "0", transform: "translateY(30px)" },
+        }
+      },
+      animation: {
+        dada: "dada 10s linear infinite 0s",
+      }
+    },
   },
   plugins: [],
 }
